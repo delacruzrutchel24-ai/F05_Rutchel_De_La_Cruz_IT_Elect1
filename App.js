6b9e77e7f65d69ef959a53ab6c3886c22834b0f7
Rutchel De La Cruz <delacruzrutchel24@gmail.com>
2025-09-20 11:22:24 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ScrollView } from "react-native";
+import CounterApp from "./CounterApp";
+import ColorChangerApp from "./ColorChangerApp";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <CounterApp />
+      <ColorChangerApp />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
